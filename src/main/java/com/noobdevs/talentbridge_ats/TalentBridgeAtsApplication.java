178499697c9d6272a8abe9385e09e42ebdd03ebd
Nadeen-Gunathilake <nadeen.guna05@gmail.com>
2026-07-15 21:47:26 +0530

    Initial Commit

---
+++ src/main/java/com/noobdevs/talentbridge_ats/TalentBridgeAtsApplication.java
@@ -0,0 +1,13 @@
+package com.noobdevs.talentbridge_ats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentBridgeAtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentBridgeAtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noobdevs/talentbridge_ats/TalentBridgeAtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noobdevs.talentbridge_ats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentBridgeAtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
